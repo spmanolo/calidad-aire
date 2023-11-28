3bc50f2e5b85b5f1c519b9b56b2c2d0f5cec07b1
spmanolo <manolosan2001@gmail.com>
2023-11-28 11:02:25 +0100

    Añadida lógica para obtener la media de los datos closes #24 La lógica de negocio se basa en el cálculo de la media de las mediciones de contaminación de un día en una zona específica. Añadidos métodos de clase para obtener los datos de medición de una zona en concreto y para calcular la media de esos datos. El nuevo constructor de clase se encarga de almacenar los datos obtenidos de la lectura del fichero. Además se han añadido getters que facilitan la representación de la información obtenida.

--- src/calidad_del_aire.ts
+++ src/calidad_del_aire.ts
@@ -1,28 +1,96 @@
 import { Zona } from "./zona";
 import { MedicionesDeContaminacion } from "./mediciones_de_contaminacion";
-
-/**
- * Clase calidadDelAire.
- * Entidad.
- */
-class calidadDelAire {
+import { DatosDeMedicion } from "./datos_de_medicion";
+export class CalidadDelAire {
   private dia: Date;
   private zona: Zona;
   private contaminacion: Array<MedicionesDeContaminacion>;
 
-  /**
-   * Constructor de la clase calidadDelAire.
-   * @param dia
-   * @param zona
-   * @param contaminacion
-   */
-  constructor(
-    dia: Date,
-    zona: Zona,
-    contaminacion: Array<MedicionesDeContaminacion>
-  ) {
+  constructor(dia: Date, zona: Zona, mediciones: DatosDeMedicion) {
     this.dia = dia;
     this.zona = zona;
-    this.contaminacion = contaminacion;
+    let infoMediciones: string | null = mediciones.getMediciones();
+
+    if (infoMediciones) {
+      this.contaminacion = this.extraerMediciones(infoMediciones);
+    } else {
+      this.contaminacion = [];
+    }
+  }
+
+  public getMedicionesContaminacion(): Array<MedicionesDeContaminacion> {
+    return this.contaminacion;
+  }
+
+  public getZona(): Array<string> {
+    return this.zona.getZona();
+  }
+
+  public extraerMediciones(
+    infoMediciones: string
+  ): Array<MedicionesDeContaminacion> {
+    let contaminacion: Array<MedicionesDeContaminacion> = [];
+    let lineas: Array<string> = infoMediciones.split("\n");
+
+    lineas.shift();
+    lineas.pop();
+
+    lineas.forEach((linea) => {
+      let elementos_linea: Array<string> = linea.split(";");
+      let zona_linea = new Zona(
+        elementos_linea[0],
+        elementos_linea[1],
+        elementos_linea[2]
+      );
+
+      if (
+        zona_linea
+          .getZona()
+          .every((zona, index) => zona === this.zona.getZona()[index])
+      ) {
+        let medicion = new MedicionesDeContaminacion(
+          Number(elementos_linea[5]),
+          Number(elementos_linea[6]),
+          Number(elementos_linea[7]),
+          Number(elementos_linea[8]),
+          Number(elementos_linea[9])
+        );
+        contaminacion.push(medicion);
+      }
+    });
+
+    return contaminacion;
+  }
+
+  // hacer la media de las mediciones de contaminación
+  public calcularMediaContaminacion(): Array<[string, number]> {
+    let totalPM10 = 0;
+    let totalPM25 = 0;
+    let totalNO2 = 0;
+    let totalO3 = 0;
+    let totalSO2 = 0;
+
+    // sumar los valores de cada contaminante
+    this.contaminacion.forEach((medicion) => {
+      const medicionesTotales: Array<[string, number]> =
+        medicion.getMediciones();
+
+      totalPM10 += medicionesTotales[0][1];
+      totalPM25 += medicionesTotales[1][1];
+      totalNO2 += medicionesTotales[2][1];
+      totalO3 += medicionesTotales[3][1];
+      totalSO2 += medicionesTotales[4][1];
+    });
+
+    // terminar de hacer la media y parsear a dos decimales
+    const media: MedicionesDeContaminacion = new MedicionesDeContaminacion(
+      parseFloat((totalPM10 / this.contaminacion.length).toFixed(2)),
+      parseFloat((totalPM25 / this.contaminacion.length).toFixed(2)),
+      parseFloat((totalNO2 / this.contaminacion.length).toFixed(2)),
+      parseFloat((totalO3 / this.contaminacion.length).toFixed(2)),
+      parseFloat((totalSO2 / this.contaminacion.length).toFixed(2))
+    );
+
+    return media.getMediciones();
   }
 }
--- src/mediciones_de_contaminacion.ts
+++ src/mediciones_de_contaminacion.ts
@@ -1,7 +1,3 @@
-/**
- * Clase MedicionesDeContaminacion.
- * Objeto valor que representa las mediciones de la contaminación de una localización.
- */
 export class MedicionesDeContaminacion {
   private pm10: number;
   private pm25: number;
@@ -9,14 +5,6 @@ export class MedicionesDeContaminacion {
   private o3: number;
   private so2: number;
 
-  /**
-   * Constructor de la clase MedicionesDeContaminacion.
-   * @param pm10 Medición de PM10.
-   * @param pm25 Medición de PM2.5.
-   * @param no2 Medición de NO2.
-   * @param o3 Medición de O3.
-   * @param so2 Medición de SO2.
-   */
   constructor(
     pm10: number,
     pm25: number,
@@ -29,5 +17,15 @@ export class MedicionesDeContaminacion {
     this.no2 = no2;
     this.o3 = o3;
     this.so2 = so2;
+  }
+
+  public getMediciones(): Array<[string, number]> {
+    return [
+      ["pm10", this.pm10],
+      ["pm25", this.pm25],
+      ["no2", this.no2],
+      ["o3", this.o3],
+      ["so2", this.so2],
+    ];
   }
 }
